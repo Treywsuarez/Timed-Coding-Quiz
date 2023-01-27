ad12db3d954bcc825907a0f042a49bf307fd050e
Treysuarez <treyxsuarez@gmail.com>
2023-01-27 19:23:17 +0000

    added user initials code to logic.js

--- starter/assets/js/logic.js
+++ starter/assets/js/logic.js
@@ -2,3 +2,6 @@
 var startbtn = document.querySelector("#Start");
 var submitbtn = document.querySelector("#submit");
 
+// user Initials input logic
+var initialsInput = document.querySelector("#initials")
+
