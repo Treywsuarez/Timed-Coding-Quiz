6126d2e818c7080d72f6f7985ce3001355d171c3
Treysuarez <treyxsuarez@gmail.com>
2023-01-27 19:27:51 +0000

    added quiz screen logic to logic.js

--- starter/assets/js/logic.js
+++ starter/assets/js/logic.js
@@ -5,3 +5,11 @@ var submitbtn = document.querySelector("#submit");
 // user Initials input logic
 var initialsInput = document.querySelector("#initials")
 
+// final score logic
+var finalScore = document.querySelector("#final-score");
+
+// quiz screen logic
+var startScreen = document.querySelector("#start-screen");
+var questionsScreen = document.querySelector("#questions");
+var endScreen = document.querySelector("#end-screen");
+
