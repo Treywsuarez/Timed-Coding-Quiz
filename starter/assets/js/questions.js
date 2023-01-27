fb6be3aff0ce5a31c9d664fea60a27b8dcc6942b
Treysuarez <treyxsuarez@gmail.com>
2023-01-27 19:11:43 +0000

    questions added to question.js

---
+++ starter/assets/js/questions.js
@@ -0,0 +1,42 @@
+var choices = [
+    {
+        questionTitles: "String values must be enclosed within ___ when being assigned to variables.",
+        options: ["commas", "curly brackets", "quotes", "parentheses"],
+        correctAnswer: "quotes"
+    },
+    {
+        questionTitles: "What is the correct syntax for referring to an external script called 'xxx.js'?",
+        options: ["<script href='xxx.js'>", "<script name='xxx.js'>", "<script src='xxx.js'>", "<script file='xxx.js'>"],
+        correctAnswer: "<script src='xxx.js'>"
+    },
+    {
+        questionTitles: "Which of the following is not a looping structure?",
+        options: ["For", "While", "Foreach"],
+        correctAnswer: "Foreach"
+    },
+    {
+        questionTitles: "Arrays in JavaScript can be used to store ____.",
+        options: ["numbers and strings", "other arrays", "booleans", "all can be used"],
+        correctAnswer: "all can be used"
+    },
+    {
+        questionTitles: "Inside which HTML element do we put the JavaScript?",
+        options: ["<scripting>", "<javascript>", "<js>", "<script>"],
+        correctAnswer: "<script>"
+    },
+    {
+        questionTitles: "How do you write " + '"Hello World" ' + "in an alert box?",
+        options: ['alert("Hello World")', 'msg("Hello World")', 'msgBox("Hello World")', 'alertBox("Hello World")'],
+        correctAnswer: 'alert("Hello World")'
+    },
+    {
+        questionTitles: "Which operator is used to assign a value to a variable?",
+        options: ["=", "x", "-", "*"],
+        correctAnswer: "="
+    },
+    {
+        questionTitles: "How can you add a comment in JavaScript?",
+        options: ["<!-- This is a comment -->", "// This is a comment", "' This is a comment", "- This is a comment"],
+        correctAnswer: "// This is a comment"
+    },
+]
