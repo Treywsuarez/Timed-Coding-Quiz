e80476c433e7ce882bab6a7b91f969a317f2b7be
Treysuarez <treyxsuarez@gmail.com>
2023-01-28 13:04:03 +0000

    added function to check storgae and show scores

--- starter/assets/js/scores.js
+++ starter/assets/js/scores.js
@@ -5,3 +5,32 @@ var clear = document.querySelector("#clear");
 
 var highscores = [];
 
+// LOCAL STORAGE //
+
+// check local storage at the start of the game
+
+function getScores() {
+    var storedScores = localStorage.getItem("scores")
+    // if no score stored, return nothing
+    if (storedScores === null){
+        return;
+    }else{
+        // if there are scores convert to objects
+        highscores = JSON.parse(storedScores);
+        // sort scores from highest to lowest
+        highscores.sort(function (a, b){
+            return b.score - a.score;
+        })
+    }
+
+    // show only top 5 scores
+    highscores.forEach(function (scoreObject, index){
+        if (index > 4){
+            return;
+        }
+        // list each score
+        let listScores = document.createElement("li");
+        listScores.textContent = scoreObject.initials + "" + scoreObject.score;
+        highscoreElements.appendChild(listScores);
+    })
+}
